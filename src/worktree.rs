b736a3a9dff6071782e2a6b58c09c2ac720a4faf
Nick <speedyleion@users.noreply.github.com>
2021-02-27 22:01:29 -0800

    have the mtime and the size compared

--- src/worktree.rs
+++ src/worktree.rs
@@ -12,6 +12,7 @@ use std::sync::{Mutex, Arc};
 
 use crate::DirEntry;
 use crate::Index;
+use std::time::SystemTime;
 
 #[derive(Debug)]
 enum Status {
@@ -70,7 +71,18 @@ impl WorkTree {
             .process_read_dir(process_directory);
         let mut entries = vec![];
         for entry in walk_dir {
-            entries.push(DirEntry {mtime: 0, size: 0, name: entry?.path().to_str().ok_or(WorkTreeError{message: "FAIL WHALE".to_string()})?.to_string(), ..Default::default()});
+            let entry = entry.unwrap();
+
+            // Leverage the fact that `read_children_path` is set to None for files
+            match entry.read_children_path {
+                None => {
+                    let meta = entry.metadata().unwrap();
+                    let mtime = meta.modified().unwrap().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs() as u32;
+                    let size = meta.len() as u32;
+                    entries.push(DirEntry {mtime, size, name: entry.path().to_str().ok_or(WorkTreeError{message: "FAIL WHALE".to_string()})?.to_string(), ..Default::default()})
+                },
+                _ => continue,
+            }
         }
         let work_tree = WorkTree {
             path: String::from(path.to_str().unwrap()),
@@ -110,6 +122,7 @@ mod tests {
     use super::*;
     use temp_testdir::TempDir;
     use std::fs;
+    use std::time::SystemTime;
 
     #[test]
     fn test_diff_against_index() {
@@ -118,10 +131,11 @@ mod tests {
         let entry_name = "simple_file.txt";
         let file = temp_dir.join(entry_name);
         fs::create_dir_all(file.parent().unwrap()).unwrap();
-        fs::write(file, file_contents).unwrap();
+        fs::write(&file, file_contents).unwrap();
         let index =  Index::default();
         let value = WorkTree::diff_against_index(&*temp_dir, &index).unwrap();
-        assert_eq!(value.entries.len(), 1);
+        let entries = vec![DirEntry{name: file.to_str().unwrap().to_string(), size: file_contents.len() as u32, mtime: fs::metadata(&file).unwrap().modified().unwrap().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs() as u32, ..Default::default()}];
+        assert_eq!(value.entries, entries);
 
     }
 }
