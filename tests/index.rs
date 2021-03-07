b4ebf81510efaa47cf822a20c592842aa3952938
Nick <speedyleion@users.noreply.github.com>
2021-03-06 20:25:46 -0800

    index integration tests working again

--- tests/index.rs
+++ tests/index.rs
@@ -33,14 +33,11 @@ fn index_has_three_entries_in_order() {
     let index_file = temp.join(".git/index");
     let index = Index::new(&index_file).unwrap();
 
-    // let mut index_names = HashMap::new();
-    // index.entries.into_iter().map(|(k, v)| index_names.insert(k, v.iter().map(|e| &e.name).collect())).collect();
-
-    // let mut file_map = HashMap::new();
-    // files.iter().map(|f| file_map.insert(f.parent().unwrap().to_str().unwrap(), f.to_str().unwrap())).collect();
-    // let index_names: Vec<&String> = index.entries.iter().map(|e| &e.name).collect();
-    // names.sort();
-    // assert_eq!(index_names, names);
+    assert_eq!(index.entries.len(), 1);
+    let dir_list = index.entries.get("").unwrap();
+    let index_names: Vec<&String> = dir_list.iter().map(|e| &e.name).collect();
+    names.sort();
+    assert_eq!(index_names, names);
 }
 
 #[test]
@@ -55,8 +52,15 @@ fn index_has_nested_entries_in_order() {
 
     // let mut index_names = HashMap::new();
     // index.entries.into_iter().map(|(k, v)| index_names.insert(k, v.iter().map(|e| &e.name))).collect();
-
-    // let mut file_map = HashMap::new();
-    // files.iter().map(|f| file_map.insert(f.parent().unwrap(), f.to_str().unwrap())).collect();
-    // assert_eq!(index_names.len() == file_map.len() && index_names.keys().all(|k| file_map.contains_key(k)));
+    let mut file_map = HashMap::new();
+    names.sort();
+    for file in names.iter().map(|n| Path::new(n)) {
+        let mut entry = file_map.entry(file.parent().unwrap().to_str().unwrap()).or_insert(vec![]);
+        entry.push(file.file_name().unwrap().to_str().unwrap());
+    }
+    assert_eq!(index.entries.len(), file_map.len());
+    for (key, value) in index.entries.into_iter() {
+        let index_names: Vec<&String> = value.iter().map(|e| &e.name).collect();
+        assert_eq!(&index_names, file_map.get(key.as_str()).unwrap());
+    }
 }
