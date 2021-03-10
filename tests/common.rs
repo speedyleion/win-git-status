e32f7a16b58ce6e02bb82347dfc9cacdc30ca023
Nick <speedyleion@users.noreply.github.com>
2021-03-09 19:07:12 -0800

    adding test case for submodules in worktree

--- tests/common.rs
+++ tests/common.rs
@@ -4,7 +4,7 @@
  *    (See accompanying file LICENSE or copy at
  *          https://www.boost.org/LICENSE_1_0.txt)
  */
-use git2::{Repository, Signature, Time};
+use git2::{Repository, Signature, Time, SubmoduleUpdateOptions};
 use std::fs;
 use std::path::Path;
 
@@ -34,4 +34,28 @@ pub fn test_repo(path: &str, files: Vec<&Path>) -> () {
         &[],
     )
     .unwrap();
+}
+
+pub fn add_submodule(path: &str, submodule_url: &str, submodule_path: &str) -> () {
+    let repo = Repository::init(path).unwrap();
+    let mut submodule = repo.submodule(submodule_url, Path::new(submodule_path), true).unwrap();
+    let mut submodule_options = SubmoduleUpdateOptions::new();
+    submodule.clone(Some(&mut submodule_options)).unwrap();
+    submodule.add_finalize().unwrap();
+
+    let mut index = repo.index().unwrap();
+    index.write().unwrap();
+    let tree_oid = index.write_tree().unwrap();
+    let tree = repo.find_tree(tree_oid).unwrap();
+    let signature = Signature::new("Tucan", "me@me.com", &Time::new(20, 0)).unwrap();
+    let head = repo.head().unwrap().target().unwrap();
+    let head = repo.find_commit(head).unwrap();
+    repo.commit(
+        Option::from("HEAD"),
+        &signature,
+        &signature,
+        "Adding submodule",
+        &tree,
+        &[&head],
+    ).unwrap();
 }
---
+++ tests/worktree.rs
@@ -0,0 +1,36 @@
+/*
+ *          Copyright Nick G. 2021.
+ * Distributed under the Boost Software License, Version 1.0.
+ *    (See accompanying file LICENSE or copy at
+ *          https://www.boost.org/LICENSE_1_0.txt)
+ */
+
+extern crate win_git_status;
+use std::collections::HashMap;
+use std::path::Path;
+use temp_testdir::TempDir;
+use win_git_status::{Index, WorkTree};
+
+mod common;
+
+#[test]
+fn worktree_diff_with_submodule() {
+    let temp = TempDir::default().permanent();
+    let super_repo = temp.join("super_repo");
+    let names = vec!["a_file.txt", "another.md", "what.log"];
+    let files = names.iter().map(|n| Path::new(n)).collect();
+    common::test_repo(super_repo.to_str().unwrap(), files);
+
+    let sub_repo = temp.join("sub_repo");
+    let sub_names = vec!["a_sub_file.md", "sure.c"];
+    let sub_files = sub_names.iter().map(|n| Path::new(n)).collect();
+    common::test_repo(sub_repo.to_str().unwrap(), sub_files);
+
+    common::add_submodule(super_repo.to_str().unwrap(), sub_repo.to_str().unwrap(), "sub_repo_dir");
+
+    let index_file = super_repo.join(".git/index");
+    let index = Index::new(&index_file).unwrap();
+
+    let value = WorkTree::diff_against_index(&super_repo, index).unwrap();
+    assert_eq!(value.entries, vec![]);
+}
